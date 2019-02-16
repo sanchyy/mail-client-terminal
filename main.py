2a4107f145c4826c87de4816059e3ed61ce5ac74
Sanchy <vsanchez.gassull@gmail.com>
2019-02-16 09:41:16 +0100

    view inbox

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from '.' import mail
 import json
+import os
+from email.mime.text import MIMEText
+import smtplib
+import imaplib
+import getpass
 
-#service = json a carregar
+PORT = 587
 
 def services():
-    with open(services.json,'w') as f:
+    with open('services.json','w') as f:
         data = json.load(f)
         for k,v in data:
             print('[{}] {}'.format(k,v))
@@ -18,23 +22,67 @@ def services():
 
         return data[opt]
             
+
+def set_user():
+    _user = User()
+    user.mail = input('mail: ')
+    user.password = getpass.getpass('password: ')
+    return user
+
+def set_mail():
+    mail = Mail()
+    mail.service = services()
+    mail.user = user
+    mail.sender = str(input("A qui vols enviar l'e-mail"))
+    with open('msg_to_{}.txt'.format(mail['To'].split('@')[0]),'w') as f:
+        f.write('SUBJECT: ')
+    os.system('vi msg_to_{}.txt'.format(mail['To'].split('@')[0])) 
+    with open('msg_to_{}.txt'.format(mail.sender.split('@')[0]),'w') as f:
+        mail.subj = f.readline()
+        message = f.read().splitlines(True)
+        mail.msg = message[1:]
+    return mail
     
 
+def send_mail(m):
+    mailserver = smtplib.SMTP(m.service,PORT)
+    mailserver.ehlo()
+    mailserver.starttls()  
+    mailserver.ehlo()
+    mailserver.login(m.user.mail,m.user.password)
+    #mailserver.sendmail('')
+    mailserver.quit()
+
+def view_inbox():
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login(user.mail,user.password)
+    return  
+
 if __name__ == '__main__':
+    m = Mail()
+    user = User()
+    mail
+    
     while 1:
-        option = input("> ")
-        if option.lower() == 'help':
+        option = str(input("> ")).lower()
+        if option == 'help':
             print('what to do...')    
-        elif option.lower() == 'set-user':
-            us = input('mail: ')
-            pw = input('password: ')
-        
-        elif option.lower() == 'send-mail'
+
+        elif option == 'set-user':
+            user = set_user()
+            
+        elif option == 'send-mail':
             if user.mail == "":
                 print("define user before sending mail")
             else:
-                email.service = services()
-                email.user
-               
-                
+                m = set_mail()
+                send_mail(m)
 
+        elif: option == 'inbox':
+            if user.mail == "":
+                print("Primer fes login!")
+            else:
+               view_inbox(m)
+
+        else:
+            print('Not a possible action, type help for information')
